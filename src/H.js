1c470a9597996e7a90fedae9d51f27d91afe040b
ASSROUGUI <ashwagooo@hotmail.com>
2019-12-23 13:26:57 +0300

    initial commit

---
+++ src/H.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import HomePage from './components/HomePage';
+
+class H extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <HomePage />
+        );
+    }
+}
+
+export default H;
